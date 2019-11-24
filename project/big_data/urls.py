41651b102f5291f844654ff7073591d91a8e274c
255-1 <1911646322@qq.com>
2019-11-24 20:25:59 +0800

    Clear

--- project/big_data/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns=[
-    path('',views.test,name='test'),
-    path('form',views.get_jobinfo,name='form'),
-    path('stats',views.get_group_statistics,name='stats')
-]
