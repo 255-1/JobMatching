0a640244e7f37ab823f97ca05fb6a67f8b750a28
Gordo-li <liyi19980606@163.com>
2020-02-09 03:05:22 +0800

    职业分类

--- 职业分类/源码/done.py
+++ 职业分类/源码/done.py
@@ -14,75 +14,104 @@ import jieba
 import pickle
 import glob
 import time
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 from sklearn import metrics
 from threading import Thread, Lock
 from queue import Queue
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.datasets.base import Bunch
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+import warnings
+warnings.filterwarnings('ignore')  # "error", "ignore", "always", "default", "module" or "once"
 
 class func_tools(object):
+    '''
+    工具类
+    '''
     def __init__(self):
         pass
 
     def tokenizer(self):
         return jieba
 
-    def readfile(self, filepath, encoding='utf-8'):
+    def readfile(self, file_path, encoding='utf-8'):
         '''
-        读取文本内容
-        :param filepath: 文本地址（博文中有小错，应为txt文件）
-        :param encoding: 打开文件格式
-        :return: filepath.txt的内容
+        读取文本文件内容
+        :param file_path: str 文本地址
+        :param encoding: str 打开文件格式
+        :return: str file_path下文本文件内容
         '''
-        # 读取文本
-        with open(filepath, "rt", encoding=encoding) as fp:
+        with open(file_path, "rt", encoding=encoding) as fp:
             content = fp.read()
         return content
 
     def savefile(self, savepath, content):
         '''
-        保存文本
-        :param savepath:
-        :param content:
+        保存文本到指定路径
+        :param savepath: str 保存路径
+        :param content: str 文本文件内容
         :return:
         '''
-        # 保存文本
         with open(savepath, "wt") as fp:
             fp.write(content)
 
     def writeobj(self, path, obj):
-        # 持久化python对象
-        # print(obj)
+        '''
+        持久化python对象
+        :param path: str 保存bunch文件地址
+        :param obj: Bunch 内容
+        :return:
+        '''
         with open(path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
     def readobj(self, path):
-        # 载入python对象
+        '''
+        载入python对象
+        :param path: str 文件地址
+        :return: Bunch 文件内容
+        '''
         with open(path, "rb") as file_obj:
             obj = pickle.load(file_obj)
         return obj
 
     def check_dir_exist(self, dir):
-        # 坚持目录是否存在，不存在则创建
+        '''
+        坚持目录是否存在，不存在则创建
+        :return:
+        '''
         if not os.path.exists(dir):
             os.mkdir(dir)
 
+
 class DataCut(object):
+    '''
+    将语料库的数据切分成训练集和测试集
+    '''
     def __init__(self, corpus_dir, save_path, ratio):
+        '''
+        :param corpus_dir : str 语料库文件夹路径 ./data/jobInfo.corpus
+        :param save_path: str 训练集和测试集的父目录路径 ./data/jobInfo
+        :param ratio: float 训练集/训练集的比例
+        :return:
+        '''
         self.corpus_dir = corpus_dir
         self.save_path = save_path
         self.ratio = ratio
         self.THREADLOCK = Lock()
-
+        # 判断是否完成
         self.end = []
 
-        # 检测目录是否存在，不存在则创建
-        func_tools().check_dir_exist(dir=self.corpus_dir)
-        func_tools().check_dir_exist(dir=self.save_path)
-
     def start_data_cut(self):
-        # 在./data/jobInfo/目录下建立测试集和训练集 --- 【./data/jobInfo/train】和【./data/jobInfo/test】
+        '''
+        对语料库划分测试集和训练集
+        测试集和训练集默认保存在./data/jobInfo目录下的两个目录中
+        训练集路径为：./data/jobInfo/train
+        测试集路径为：./data/jobInfo/test
+        '''
         train_folder = os.path.join(self.save_path, "train")
         test_folder = os.path.join(self.save_path, "test")
         print(train_folder)
@@ -103,7 +132,6 @@ class DataCut(object):
 
         for i in range(8):
             Thread(target=self.copyfile, args=(q,)).start()
-
         # 判断是否完成
         while len(self.end) < len(os.listdir(self.corpus_dir)):
             time.sleep(2)
@@ -111,7 +139,7 @@ class DataCut(object):
     def copyfile(self, q):
         '''
         拷贝数据到测试集和训练集目录下
-        :param q:
+        :param q: Queue 一个“队列”对象
         :return:
         '''
         while not q.empty():
@@ -130,24 +158,32 @@ class DataCut(object):
             with self.THREADLOCK:
                 print(full_folder)
 
+
 class ExtractData(object):
     '''
     提取原始Excel数据表中的数据【./data/ori_jobInfo'】，做成完整的语料库数据集【./data/jobInfo.corpus】。
     准备进行数据测试集和训练集划分
     '''
     def __init__(self, ori_jobInfo_dir, corpus_dir):
+        '''
+        :param ori_jobInfo_dir: str Excel数据表的父目录 ./data/ori_jobInfo
+        :param corpus_dir: str 语料库路径 ./data/jobInfo.corpus
+        '''
         self.ori_jobInfo_dir = ori_jobInfo_dir
         self.corpus_dir = corpus_dir
 
     def start_extract(self):
+        '''
+        提取Excel中的文本到语料库下的文本文档
+        :param excelDir: str Excel路径  eg: ./data/ori_jobInfo/Java.xlsx...
+        :param corpusDirName str 语料库路径  eg: ./data/jobInfo.corpus/Java...
+        '''
         for basefolder in os.listdir(self.ori_jobInfo_dir):
             # Mac有.DS_Store文件，跳过
             if basefolder.startswith('.DS'):
                 continue
             # 原始Excel数据集地址
             excelDir = os.path.join(self.ori_jobInfo_dir, basefolder)
-            # print(excelDir)
-
             # 语料库数据集地址
             corpusDirName = os.path.join(self.corpus_dir, basefolder[:-5])
             # 检测目录是否存在，不存在则创建
@@ -157,6 +193,11 @@ class ExtractData(object):
             self.excel_to_corpus(excel_path=excelDir, corpus_path=corpusDirName)
 
     def excel_to_corpus(self, excel_path, corpus_path):
+        '''
+        将Excel文本内容逐行保存到语料库目录下相应文件中
+        :param excel_path: str Excel路径  eg: ./data/ori_jobInfo/Java.xlsx.
+        :param corpus_path: str 语料库路径  eg: ./data/jobInfo.corpus/Java
+        '''
         readExcel = xlrd.open_workbook(excel_path)
         sheet = readExcel.sheet_by_name('Sheet1')
         nrows = sheet.nrows  # 返回行：100
@@ -164,22 +205,22 @@ class ExtractData(object):
         for row in range(nrows-1):
             # 测试都放在一个txt文本中
             txt_path = os.path.join(corpus_path, str(row)+'.txt')     # 0.txt ~ XX.txt
-
             with open(txt_path, "w") as file:
                 file.write(sheet.cell(row+1, 0).value)
 
+
 class JobInfoSeg(object):
+    '''
+    对文本分词
+    '''
     def __init__(self):
-        # userdict = './userdict.txt'
-        # jieba.load_userdict(userdict)
         self.LOCK = Lock()
         self.end = []
 
-
     def text_segment(self, q):
-        """
+        '''
         对一个类别目录下进行分词
-        """
+        '''
         while not q.empty():
             from_dir, to_dir = q.get()
             with self.LOCK:
@@ -196,11 +237,11 @@ class JobInfoSeg(object):
                 func_tools().savefile(to_file, ' '.join(seg_content))
             self.end.append('end')
 
-
-
     def corpus_seg(self, curpus_path, seg_path):
         """
         对文本库分词，保存分词后的文本库,目录下以文件归类 curpus_path/category/1.txt, 保存为 seg_path/category/1.txt
+        :param corpus_dir : str 语料库文件夹路径  eg: ./data/jobInfo.corpus/train 或/test
+        :param seg_path: str 分词后保存路径 eg: ./data/jobInfo.corpus/train_seg 或/test_seg
         """
         func_tools().check_dir_exist(seg_path)
         q = Queue()
@@ -211,27 +252,38 @@ class JobInfoSeg(object):
             func_tools().check_dir_exist(to_dir)
 
             q.put((from_dir, to_dir))
-
         for i in range(len(cat_folders)):
             Thread(target=self.text_segment, args=(q,)).start()
         # 判断是否完成
         while len(self.end) < len(cat_folders):
             time.sleep(2)
 
+
 class Tfidf_Feature(object):
+    '''
+    特征提取
+    用到 sklearn 特征提取模块的 TfidfVectorizer 类。构建词袋对象，将特征矩阵保存为 sklearn 的 Bunch 数据结构
+    '''
     def __init__(self):
         pass
 
     def corpus2Bunch(self, wordbag_path, seg_path):
+        '''
+        生成训练集和测试集的词袋
+        :param wordbag_path: str 词袋保存路径
+        :param seg_path: str 分词后的语料库目录路径
+        '''
         catelist = os.listdir(seg_path)  # 获取seg_path下的所有子目录，也就是分类信息
 
         # 创建一个Bunch实例
         bunch = Bunch(target_name=[], label=[], filenames=[], contents=[])
+        '''
+        target_name 类别 
+        label 对应的标签
+        filenames 文本绝对路径列表
+        contents 文本内容
+        '''
         bunch.target_name.extend(catelist)
-        '''
-        extend(addlist)是python list中的函数，意思是用新的list（addlist）去扩充
-        原来的list
-        '''
         # 获取每个目录下所有的文件
         for mydir in catelist:
             class_path = seg_path + '/' + mydir + '/'  # 拼出分类子目录的路径
@@ -241,20 +293,35 @@ class Tfidf_Feature(object):
                 bunch.label.append(mydir)
                 bunch.filenames.append(fullname)
                 bunch.contents.append(func_tools().readfile(fullname))  # 读取文件内容
-                '''append(element)是python list中的函数，意思是向原来的list中添加element，注意与extend()函数的区别'''
 
         func_tools().writeobj(wordbag_path, bunch)
         print("构建文本对象结束: " + wordbag_path)
 
     def vector_space(self, bunch_mian_path, bunch_secondary_path, stopword_path, space_save_path):
-        stpwrdlst = func_tools().readfile(stopword_path).split()  # 读取停用词
-        bunch = func_tools().readobj(bunch_mian_path)  # 导入分词后的词向量bunch对象
+        '''
+        将语料库向量化
+        :param bunch_mian_path: str 训练集或测试集的词袋
+        :param bunch_secondary_path: str 训练集的语料库词袋路径，生成测试集特征向量的需要训练集的语料库词典
+        :param stopword_path: str 停用词文件路径
+        :param space_save_path: str 特征向量保存路径
+        :param
+        '''
+        # 读取停用词
+        stpwrdlst = func_tools().readfile(stopword_path).split()
+        # 导入分词后的词向量bunch对象
+        bunch = func_tools().readobj(bunch_mian_path)
         # 构建tf-idf词向量空间对象
         tfidfspace = Bunch(target_name=bunch.target_name, label=bunch.label, filenames=bunch.filenames, tdm=[],
                            vocabulary={})
-
+        '''
+        target_name 类别 
+        label 对应的标签
+        filenames 文本绝对路径列表
+        tdm 是特征矩阵
+        vocabulary 是语料库词典
+        '''
         if bunch_secondary_path:
-            '''test'''
+            '''test   生成测试集特征向量的需要训练集的语料库词典'''
             trainbunch = func_tools().readobj(bunch_secondary_path)
             tfidfspace.vocabulary = trainbunch.vocabulary
             vectorizer = TfidfVectorizer(stop_words=stpwrdlst, sublinear_tf=True, max_df=0.5,
@@ -262,23 +329,26 @@ class Tfidf_Feature(object):
             tfidfspace.tdm = vectorizer.fit_transform(bunch.contents)
         else:
             '''train'''
-            # 此时tdm里面存储的就是if-idf权值矩阵
             vectorizer = TfidfVectorizer(stop_words=stpwrdlst, sublinear_tf=True, max_df=0.5)
             tfidfspace.tdm = vectorizer.fit_transform(bunch.contents)
             tfidfspace.vocabulary = vectorizer.vocabulary_
 
         func_tools().writeobj(space_save_path, tfidfspace)
-
         print("if-idf词向量空间实例创建成功: " + space_save_path)
 
+
 class TextClassifier(object):
+    '''
+    分类器类，可以根据需要选择不同的分类算法
+    '''
     def __init__(self, clf_model, space_path, test_space_path, model_path):
-        """
+        '''
         分类器
-        :param clf_model:   分类器算法模型
-        :param data_dir:    特征数据存放位置
-        :param model_path:  模型保存路径
-        """
+        :param clf_model: str 分类器算法模型
+        :param space_path: str 训练集特征数据存放位置
+        :param test_space_path: str 测试集特征数据存放位置
+        :param model_path: str 模型保存路径
+        '''
         self.space_path = space_path
         self.test_space_path = test_space_path
         self.model_path = model_path
@@ -287,28 +357,32 @@ class TextClassifier(object):
     def _load_clf_model(self, clf_model):
         '''
         判断是否已有模型
+        :param clf_model: str 模型路径
         '''
         if os.path.exists(self.model_path):
-            print('\nloading exists model...')
+            print('loading exists model...')
             return joblib.load(self.model_path)
         else:
-            print('\ntraining model...')
+            print('training model...')
             train_set = func_tools().readobj(self.space_path)
             clf = clf_model.fit(train_set.tdm, train_set.label)
             joblib.dump(clf, self.model_path)
             return clf
 
     def validation(self):
-        """使用测试集进行模型验证"""
+        '''
+        使用测试集进行模型验证
+        '''
         print('starting validation...')
-        # 导入测试集
+        # 导入测试集特征数据 ./data/jobInfo_app/fearture/testspace.dat
         test_set = func_tools().readobj(self.test_space_path)
 
         # 预测分类结果
         predicted = self.clf.predict(test_set.tdm)
-        for flabel, file_name, expct_cate in zip(test_set.label, test_set.filenames, predicted):
-            if flabel != expct_cate:
-                print(file_name, ": 实际类别:", flabel, " -->预测类别:", expct_cate)
+        for flabel, file_name, category in zip(test_set.label, test_set.filenames, predicted):
+            if flabel != category:
+                pass
+                # print(file_name, ": 实际类别:", flabel, " -->预测类别:", category)
 
         self.metrics_result(test_set.label, predicted)
 
@@ -321,17 +395,22 @@ class TextClassifier(object):
         print('f1-score:{0:.3f}'.format(metrics.f1_score(actual, predict, average='weighted')))
 
     def predict_category(self, text_string, stop_words_path):
+        '''
+        对输入文件预测分类
+        :param text_string: str 预测文本分类
+        :param stop_words_path: str 停用词文本路径
+        :return predicted_result: str 返回文本分类结果
+        '''
         print('*********** 语句 *************')
         input_data = [text_string]
         stpwrdlst = func_tools().readfile(stop_words_path).split()  # 读取停用词
 
         # 构建tf-idf词向量空间对象
-        tfidfspace = Bunch(target_name=['a'], label=['b'], filenames=['c'], tdm=[],
-                           vocabulary={})
+        tfidfspace = Bunch(tdm=[], vocabulary={})
+
         trainbunch = func_tools().readobj(self.space_path)
         tfidfspace.vocabulary = trainbunch.vocabulary
-        vectorizer = TfidfVectorizer(stop_words=stpwrdlst, sublinear_tf=True, max_df=0.5,
-                                         vocabulary=trainbunch.vocabulary)
+        vectorizer = TfidfVectorizer(stop_words=stpwrdlst, sublinear_tf=True, max_df=0.5, vocabulary=trainbunch.vocabulary)
         tfidfspace.tdm = vectorizer.fit_transform(input_data)
 
         # 预测分类结果
@@ -346,6 +425,9 @@ class TextClassifier(object):
 
 
 class Application(object):
+    '''
+    应用
+    '''
     def __init__(self):
         ''' 目录(文件夹)后缀: _dir   文件后缀: _path '''
         # 原始数据文件夹路径
@@ -392,8 +474,15 @@ class Application(object):
         self.tfidf_space_path = os.path.join(self.fearture_space_dir, 'testspace.dat')
         # 停用词表的路径
         self.stopword_path = "./stop_words/stop_words.txt"
-        # 模型地址：./data/jobInfo/models/NBclassifier.pkl
-        self.model_path = os.path.join(self.model_dir, 'NBclassifier.pkl')
+
+        # 贝叶斯模型地址：./data/jobInfo/models/bayes_NBclassifier.pkl
+        self.bayes_model_path = os.path.join(self.model_dir, 'bayes_NBclassifier.pkl')
+        # 随机森林模型地址：./data/jobInfo/models/forest_NBclassifier.pkl
+        self.forest_model_path = os.path.join(self.model_dir, 'forest_NBclassifier.pkl')
+        # Logistic模型地址：./data/jobInfo/models/ogistic_NBclassifier.pkl
+        self.logistic_model_path = os.path.join(self.model_dir, 'logistic_NBclassifier.pkl')
+        # SVM模型地址：./data/jobInfo/models/SVM_NBclassifier.pkl
+        self.SVM_model_path = os.path.join(self.model_dir, 'SVM_NBclassifier.pkl')
 
         # 训练分类器迭代次数,alpha越小，迭代次数越多，精度越高
         self.alpha_num = 0.00001
@@ -410,6 +499,9 @@ class Application(object):
         print('训练集测试集划分完成')
 
     def jieba_seg(self):
+        '''
+        jieba 分词
+        '''
         print('--------------- jieba 分词 --------------')
         # 未分成的训练集和测试集
         ori_train_dir = os.path.join(self.jobInfo_dir, 'train')
@@ -435,23 +527,67 @@ class Application(object):
                                      stopword_path=self.stopword_path, space_save_path=self.tfidf_space_path)
 
     def create_model(self):
+        '''
+        训练模型
+        '''
         # 多项式贝叶斯算法训练模型
-        print('--------------- 多项式贝叶斯算法训练模型 --------------')
+        print('--------------- 训练模型 --------------')
         from sklearn.naive_bayes import MultinomialNB
-        # 训练分类器：输入词袋向量和分类标签，alpha:0.001 alpha越小，迭代次数越多，精度越高
-        clf = MultinomialNB(alpha=self.alpha_num)
+
         # 模型存放的文件夹
         func_tools().check_dir_exist(dir=self.model_dir)
-        classifier = TextClassifier(clf_model=clf, space_path=self.space_path, test_space_path=self.tfidf_space_path, model_path=self.model_path)
+
+        print('\n贝叶斯算法训练模型')
+        # 贝叶斯算法训练模型
+        # 训练分类器：输入词袋向量和分类标签，alpha:0.001 alpha越小，迭代次数越多，精度越高
+        clf = MultinomialNB(alpha=self.alpha_num)
+        classifier = TextClassifier(clf_model=clf, space_path=self.space_path, test_space_path=self.tfidf_space_path, model_path=self.bayes_model_path)
         classifier.validation()
 
-    def use_classification(self, text_dir=None, text_string=None):
+        print('\n随机森林算法算法训练模型')
+        # 随机森林算法算法训练模型
+        clf = RandomForestClassifier(bootstrap=True, oob_score=True, criterion='gini')
+        classifier = TextClassifier(clf_model=clf, space_path=self.space_path, test_space_path=self.tfidf_space_path, model_path=self.forest_model_path)
+        classifier.validation()
+
+        print('\nLogistic回归模型')
+        # Logistic回归模型
+        clf = LogisticRegression(C=1000.0)
+        classifier = TextClassifier(clf_model=clf, space_path=self.space_path, test_space_path=self.tfidf_space_path, model_path=self.logistic_model_path)
+        classifier.validation()
+
+        print('\nSVM模型')
+        # SVM模型
+        clf = SVC(kernel='linear')
+        classifier = TextClassifier(clf_model=clf, space_path=self.space_path, test_space_path=self.tfidf_space_path, model_path=self.SVM_model_path)
+        classifier.validation()
+
+    def use_classification(self, text_dir=None, text_string=None, model=None):
+        '''
+        对输入文本进行分类
+        :param text_string: str 对一个字符串文本预测分类
+        :param text_dir: str 对文本文档内容分类
+        :param model: str 使用模型选择 model = 'bayes' 'forset' 'logistic' 'SVM'
+        :return : str 返回预测类别结果
+        '''
+        if model == 'bayes':
+            model_path = self.bayes_model_path
+        elif model == 'forest':
+            model_path = self.forest_model_path
+        elif model == 'logistic':
+            model_path = self.logistic_model_path
+        elif model == 'SVM':
+            model_path = self.SVM_model_path
+        else:
+            model = 'bayes'
+            model_path = self.bayes_model_path
+        print('\n采用', model)
+
         if text_string:
             seg_text_string = " ".join(jieba.cut(text_string, cut_all=False))
             from sklearn.naive_bayes import MultinomialNB
-            classifier = TextClassifier(clf_model=MultinomialNB(alpha=self.alpha_num), space_path=self.space_path, test_space_path=self.tfidf_space_path,
-                                        model_path=self.model_path)
-
+            classifier = TextClassifier(clf_model=MultinomialNB(alpha=self.alpha_num), space_path=self.space_path,
+                                        test_space_path=self.tfidf_space_path, model_path=model_path)
             ret = classifier.predict_category(text_string=seg_text_string, stop_words_path=self.stopword_path)
             return ret
         elif text_dir:
@@ -470,7 +606,8 @@ if __name__ == "__main__":
     app.create_model()
 
     print('--------------- 模型应用 --------------')
-    ret = app.use_classification(text_string='熟悉软件开发流程，熟练掌握SVN、Maven等开发和协同工具')
+    ret = app.use_classification(text_string='熟悉软件开发流程，熟练掌握SVN、Maven等开发和协同工具', model='logistic')
+    # model = 'bayes' 'forset' 'logistic' 'SVM'
     print('return:', ret)
 
 
