d50007122649fd6ddcbc01a33daa7a8f8afb9901
255-1 <1911646322@qq.com>
2020-03-13 23:24:38 +0800

    2.2项目代码提交

---
+++ project_v2.2/big_data/dashBoard.py
@@ -0,0 +1,190 @@
+from big_data.static import *
+from copy import  deepcopy
+
+#获取今天日期
+today = datetime.datetime.now()
+month = str(today.month)
+# day = str(today.day)##调试方便，注释
+day="5"
+#获取7天前日期
+lastWeek = today+datetime.timedelta(days=-7)
+lastMonth = str(lastWeek.month)
+lastDay = str(lastWeek.day)
+
+def transDateToMonthDay(df):
+    '''
+    将dataframe的date属性转为非格式化的月-日形式
+    eg：2020-03-06->3-6
+    :param df: 带有date属性的dataframe
+    :return: 转变后的dataframe
+    '''
+    df['date'] = df['date'].apply(lambda x: str(int(x.split("-")[1])) + "-" + str(int(x.split("-")[2])))
+    return df
+
+def transDateToMonth(df):
+    '''
+    将dataframe的date属性转为非格式化的月形式
+    eg：2020-03-06->3
+    :param df: 带有date属性的dataframe
+    :return: 转变后的dataframe
+    '''
+    df['date'] = df['date'].apply(lambda x: int(x.split("-")[1]))
+    return df
+
+def getDashBoardDataToDF(jobinfo):
+    '''
+    专门给仪表盘的数据，需要unifyName和salary，并且将salary转为平均工资
+    :return: dataframe["salary(平均值后的)","unifyName"]
+    '''
+    querySet = jobinfo.values("salary", "unifyName","date")
+    df = pd.DataFrame(list(querySet))
+    # 工资列调整为每个offer的平均工资
+    df['salary'] = df['salary'].apply(lambda x: np.mean(list(map(float, x.split("-")))))
+    return df
+
+#1
+def topSalary(df):
+    '''
+    平均工资前5的职位
+    :return:top5UnifyNameList 平均前五职位列表
+            top5AverageList 前五职位对应的平均工资,保留小数点后2位，单位万/月
+            top5OfferNumberList 前五对应的offer数量
+    '''
+    df = deepcopy(df)
+    #获取所有的职位名以及对应的offer数量和平均工资
+    unifyNameDict = {}
+    groupByUnifyName = df.groupby(by="unifyName")
+    for unifyName, groupDF in groupByUnifyName:
+        unifyNameDict[unifyName] = []
+        avgSalary = np.mean(groupDF["salary"])
+        unifyNameDict[unifyName].append(round(avgSalary,2))
+        unifyNameDict[unifyName].append(len(groupDF))
+    #排序得到top5工资
+    top5TupleList = sorted(unifyNameDict.items(),key=lambda entry:entry[1][0],reverse=True)[0:5]
+    #提取成列表
+    top5UnifyNameList=[]
+    top5AvgSalaryList=[]
+    top5OfferNumberList=[]
+    for t in top5TupleList:
+        top5UnifyNameList.append(t[0])
+        top5AvgSalaryList.append(t[1][0])
+        top5OfferNumberList.append(t[1][1])
+    print(top5UnifyNameList)
+    print(top5AvgSalaryList)
+    print(top5OfferNumberList)
+    return top5UnifyNameList,top5AvgSalaryList,top5OfferNumberList
+
+#2
+def offerNumberIncreaseTop5InWeek(df):
+    '''
+    一周offer增量前5
+    :return: unifyNameList 职位列表
+            increaseNumberList 职位对应增量的offer数
+            offerNumberList 职位对应的offer总数
+    '''
+    df = deepcopy(df)
+
+    # 日期列调整为月-日类型
+    df = transDateToMonthDay(df)
+    #筛选出今天和7天前数据
+    dfToday = df.loc[df['date']==month+"-"+day]
+    dfLastWeek = df.loc[df['date']==lastMonth+"-"+lastDay]
+    #按照职位名分组
+    todayGroupByUnifyName = dfToday.groupby(by="unifyName")
+    lastGroupByUnifyName = dfLastWeek.groupby(by="unifyName")
+    #提取数据
+
+    unifyNameDict={} #unifyName:[increaseNumber, offerNumber]
+    #后缀1为今天，后缀2为一周前的
+    for unifyName1, groupDF1 in todayGroupByUnifyName:
+        for unifyName2, groupDF2 in lastGroupByUnifyName:
+            if(unifyName1==unifyName2):
+                unifyNameDict[unifyName1] = []
+                unifyNameDict[unifyName1].append(len(groupDF1)-len(groupDF2))
+                unifyNameDict[unifyName1].append(len(groupDF1)+len(groupDF2))
+    # 排序得到top5增量
+    top5TupleList = sorted(unifyNameDict.items(), key=lambda entry: entry[1][0], reverse=True)[0:5]
+    #提取元组中的数据
+    unifyNameList = []
+    increaseNumberList = []
+    offerNumberList = []
+    for t in top5TupleList:
+        unifyNameList.append(t[0])
+        increaseNumberList.append(t[1][0])
+        offerNumberList.append(t[1][1])
+    print(unifyNameList)
+    print(increaseNumberList)
+    print(offerNumberList)
+    return unifyNameList, increaseNumberList, offerNumberList
+
+
+#3
+def avgSalryInMonth(df):
+    '''
+    当月所有unifyName的平均薪水
+    :return: unifyNameList职位列表
+            avgSalaryList职位对应的平均工资,保留小数点后2位，单位万/月
+    '''
+    df = deepcopy(df)
+    #筛选出当月的数据
+    df = transDateToMonth(df)
+    df = df.loc[df['date']==today.month]
+    #获取职位列表和职位对应的平均工资
+    unifyNameList = []
+    avgSalaryList = []
+    groupByUnifyName = df.groupby(by="unifyName")
+    for unifyName, groupDF in groupByUnifyName:
+        unifyNameList.append(unifyName)
+        avgSalary = np.mean(groupDF["salary"])
+        avgSalaryList.append(round(avgSalary,2))
+    print(unifyNameList)
+    print(avgSalaryList)
+    return unifyNameList,avgSalaryList
+
+#4
+def avgSalaryEveryMonth(df):
+    '''
+    返回每个月全部offer的平均薪资
+    :return:dateList 月份列表
+            avgSalaryList 月份列表对应的平均薪资
+    '''
+    df = deepcopy(df)
+    #调整date为月
+    df = transDateToMonth(df)
+    #获取数据
+    dateList = []
+    avgSalaryList = []
+    groupByDate = df.groupby(by="date")
+    for date, groupDF in groupByDate:
+        dateList.append(date)
+        avgSalary = np.mean(groupDF['salary'])
+        avgSalaryList.append(round(avgSalary, 2))
+    print(dateList)
+    print(avgSalaryList)
+    return dateList, avgSalaryList
+
+
+
+
+#5
+def offerNumberPercentChangeBetweenLastMonthAndThisMonth(df):
+    '''
+    今天和上个月同日的offer数量以及变化率
+    :return:percentChange 变化率（%）
+            lastMonthOfferNumber 上月同日的offer数量
+            thisMonthOfferNumber 今天的offer数量
+    '''
+    df = deepcopy(df)
+    #获取这个月和上个月日期
+    lastMonth = str(int(month)-1 if month!=1 else 12)
+    # 日期列调整为月-日类型
+    df = transDateToMonthDay(df)
+    # 筛选数据
+    thisMonthOfferNumber = len(df.loc[df['date'] == month+"-"+day])
+    lastMonthOfferNumber = len(df.loc[df['date'] == lastMonth+"-"+day])
+    percentChange=round(thisMonthOfferNumber/lastMonthOfferNumber,3)*100
+    print(thisMonthOfferNumber)
+    print(lastMonthOfferNumber)
+    print(percentChange)
+    return percentChange, lastMonthOfferNumber, thisMonthOfferNumber
+
