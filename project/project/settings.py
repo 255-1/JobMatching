5f221d917803ab843a635974a5e9cd8575ca6b6b
Xkimnt <574231683@qq.comm>
2020-04-10 20:58:01 +0800

    修复了应届生模式点击不变色的问题

--- project/project/settings.py
+++ project/project/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "djangodb",
+        'NAME': "job",
         'USER':"root",
-        'PASSWORD':'Woshishabi2813',
+        'PASSWORD':'super_rr',
         'HOST':'127.0.0.1',
         'PORT':'3306'
     }
